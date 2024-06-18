d6116d125f764296bdaaa08f2697ca8881e995a7
Imbol <mari_imbol@hotmail.com>
2024-06-18 17:33:14 -0500

    news first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import NewsBoard from './Components/NewsBoard'
+
+export const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <NewsBoard/>
+    </div>
+  )
+}
+
+export default App
